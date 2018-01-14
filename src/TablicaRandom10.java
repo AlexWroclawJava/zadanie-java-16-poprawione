b530d49d38fce6fe673e174127bac81d36db44f3
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2018-01-14 19:50:01 +0100

    zadanie-java-16-poprawione

--- src/TablicaRandom10.java
+++ src/TablicaRandom10.java
@@ -10,12 +10,12 @@ public class TablicaRandom10 {
         int[] tablica = new int[10];
         int l = tablica.length;
 
-        for (int i = 0; i < tablica.length; i++) {
+        for (int i = 0; i < l; i++) {
            tablica[i] = rand.nextInt(100);
         }
 
-        for (int i = 10; i > 0; i--) {
-            System.out.print(tablica[10-i] + " ");
+        for (int i = l; i > 0; i--) {
+            System.out.print(tablica[l-i] + " ");
             System.out.print(tablica[i-1] + " ");
         }
     }
