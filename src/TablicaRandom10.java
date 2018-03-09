3210c30dd3b7a5cc93e18072b05e348cd855f69b
AlexWroclawJava <aleksander.szlachcic@gmail.com>
2018-03-09 16:23:01 +0100

    zadanie-java-16-poprawione2-z-dobrym-wyswietlaniem

--- src/TablicaRandom10.java
+++ src/TablicaRandom10.java
@@ -7,16 +7,25 @@ public class TablicaRandom10 {
 
         Random rand = new Random();
 
-        int[] tablica = new int[10];
-        int l = tablica.length;
+        int[] tablica = new int[5];
+        int length = tablica.length;
 
-        for (int i = 0; i < l; i++) {
-           tablica[i] = rand.nextInt(100);
+        for (int i = 0; i < length; i++) {
+            tablica[i] = rand.nextInt(100);
         }
+        System.out.println(Arrays.toString(tablica));
 
-        for (int i = l; i > 0; i--) {
-            System.out.print(tablica[l-i] + " ");
-            System.out.print(tablica[i-1] + " ");
+            String reversed = "";
+            for (int i = 0; i < length; i++) {
+                System.out.print(tablica[i] + " ");
+                if (reversed.isEmpty()){
+                    reversed = "" + tablica[length-1-i];
+                } else {
+                reversed = reversed + " " + tablica[length-1-i];
+                }
+            }
+            System.out.println(reversed);
         }
     }
-}
+
+
